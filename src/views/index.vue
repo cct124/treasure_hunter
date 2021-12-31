54966a437ec951e93d0401244d2a507541ced75a
huangdonghao <huangdonghao@miya818.com>
2021-12-31 14:08:45 +0800

    初始化项目结构

---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="index"></div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class Index extends Vue {}
+</script>
+<style lang="scss" scoped></style>
